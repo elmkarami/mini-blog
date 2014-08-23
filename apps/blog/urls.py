025833762e640c4ca864a9680d5a9aba5d6afeba
elmehdikarami <karami.elmehdi@gmail.com>
2014-08-23 22:05:17 +0100

    add login/logout

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,7 +4,9 @@ import views
 
 
 urlpatterns = patterns('',
-
+	
+	url(r'login/?$', views.LoginView.as_view(), name='login-user'),
+    url(r'logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout-user'),
     url(r'^$', views.ListTweets.as_view(), name='tweet-list'),
 
 )
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,15 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect
+
+from django.core.urlresolvers import reverse
+
 from django.views.generic import ListView
+from django.views.generic.base import View
+
+from django.contrib.auth import login
 
 from apps.blog.models import Tweet
+from apps.blog.forms import LoginForm
 
 
 class ListTweets(ListView):
@@ -13,3 +22,22 @@ class ListTweets(ListView):
 
     def get_queryset(self):
         return super(ListTweets, self).get_queryset()
+
+
+class LoginView(View):
+    """
+    Class based view that handle user authentication
+    """
+    form_class = LoginForm
+    template_name = 'blog/login.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'form': self.form_class()})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(None, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return HttpResponseRedirect(reverse('blog:tweet-list'))
+        return render(request, self.template_name, {'form': form})
+
