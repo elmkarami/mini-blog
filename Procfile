3fdf1eaca958c3c0fc6c13a837a91f33e6df6995
elmehdikarami <karami.elmehdi@gmail.com>
2014-08-24 21:10:22 +0100

    Add Fabfile and Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tweets.wsgi --log-file -
---
+++ fabfile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This starts out pretty simple -- just automating deployment on a Heroku
+server with a few commands.
+"""
+
+from fabric.operations import local
+
+def deploy(msg):
+    "Full deploy: push to Heroku"
+    update_dependencies
+    local("git add . -A")
+    local('git commit -am  "%s"' % msg)
+    local("git push heroku master")
+        
+def update_dependencies():
+    """Update deedel's requirements locally."""
+    local('pip freeze -> requirements.txt')
+        
