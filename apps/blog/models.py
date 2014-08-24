cef314006d59f1751783844d65ab71edfbbf2534
elmehdikarami <karami.elmehdi@gmail.com>
2014-08-24 02:11:02 +0100

    add some tests

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -4,16 +4,30 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class TweetManager(models.Manager):
+    """
+    Custom Manager for Tweet
+    """
 
     def get_query_set(self, *args, **kwargs):
+        """
+        Override the default behaviour to prevent additional DB queries
+        when accessing to the tweet's user
+        """
         return super(TweetManager, self).get_query_set(*args, **kwargs).select_related()
 
 
 class Tweet(models.Model):
+    """
+    Each Tweet contains :
+    `user`: The owner
+    `message`: The content of the tweet
+    `created`: Date of creation, filled up automatically
+    """
     user = models.ForeignKey(User, related_name='tweets')
     message = models.CharField(_('Tweet message'), max_length=140)
     created = models.DateTimeField(auto_now_add=True)
 
+    # Override the Manager
     objects = TweetManager()
 
     class Meta:
--- apps/blog/tests.py
+++ apps/blog/tests.py
@@ -4,13 +4,77 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+from model_mommy import mommy
 
+from django.contrib.auth.models import User
+
+from django.core.urlresolvers import reverse
+
+from django.test.client import Client
 from django.test import TestCase
 
+from apps.blog.models import Tweet
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+class ListTweetsViewTest(TestCase):
+    def setUp(self):
+        self.first_tweet = mommy.make(Tweet)
+        self.tweets = mommy.make(Tweet, _quantity=20)
+
+    def test_listing_tweets_with_no_tweet(self):
+        Tweet.objects.all().delete()
+        client = Client()
+        response = client.get(reverse('blog:tweet-list'))
+        self.assertIn('No Tweet available', response.content)
+
+    def test_listing_tweets(self):
+        client = Client()
+        response = client.get(reverse('blog:tweet-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Tweet.objects.all().count(), 21)
+
+    def test_order_by_created(self):
+        client = Client()
+        response = client.get(reverse('blog:tweet-list'))
+        self.assertNotIn(self.first_tweet.message, response.content)
+
+    def test_pagination_tweets(self):
+        client = Client()
+        response = client.get('%s?page=%s' % (reverse('blog:tweet-list'), 2))
+        self.assertIn(self.first_tweet.message, response.content)
+
+
+class ListTweetsByUserViewTest(TestCase):
+    def setUp(self):
+        self.first_user = mommy.make(User)
+        self.second_user = mommy.make(User)
+        mommy.make(Tweet, user=self.first_user, _quantity=1)
+        mommy.make(Tweet, user=self.second_user, _quantity=1)
+
+    def test_list_tweets_by_user(self):
+        client = Client()
+        response = client.get(reverse('blog:user-tweet-list', args=[self.first_user.pk]))
+        self.assertIn(self.first_user.tweets.all()[0].message, response.content)
+        self.assertNotIn(self.second_user.tweets.all()[0].message, response.content)
+
+
+class CreateTweetViewTest(TestCase):
+
+    def test_add_tweet_by_anonymous_user(self):
+        client = Client()
+        client.post(reverse('blog:tweet-create'), {'message':'test tweet'})
+        self.assertEqual(Tweet.objects.all().count(), 0)
+
+    def new_user(self):
+        user = mommy.prepare(User, username='karami')
+        user.set_password('mehdi')
+        user.save()
+        self.user = user
+
+    def test_add_tweet_by_authenticated_user(self):
+        self.new_user()
+        client = Client()
+        client.login(username='karami', password='mehdi')
+        client.post(reverse('blog:tweet-create'), {'message':'test tweet'})
+        self.assertEqual(Tweet.objects.filter(user=self.user).count(), 1)
+
